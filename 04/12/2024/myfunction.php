26b08fa8795c70b13305ea9b34053d1754b33b40
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-12-06 17:07:28 +0000

    updated

--- 04/12/2024/myfunction.php
+++ 04/12/2024/myfunction.php
@@ -1,30 +1,23 @@
 <?php 
 function PrintTable($student,$connect)
 {
-
 	try {
-
-	//Query
-		$sql = "SELECT * FROM $tableName";
-
-	//excute the quey
+		$sql = "SELECT * FROM $student";
 		$result = mysqli_query($connect,$sql);
-	//check if data exists in the table
+	
 		if (mysqli_num_rows($result)>0) {
-		// fetch the data from rows
+		
 			echo "<table border='1'>";
 			$col = mysqli_fetch_fields($result);
-		//print the colums
+		
 			echo "<tr>";
 			foreach ($col as $value) {
-			//return an object
-			//print_r($value);
+			
 				echo "<td>$value->name</td>";
 			}
 			echo "</tr>";
 			
 			while ($row = mysqli_fetch_assoc($result)) {
-			//print the data in a table format
 				echo "<tr>";
 				foreach ($row as $key => $value) {
 					echo "<td>$value</td>";
@@ -40,37 +33,28 @@ function PrintTable($student,$connect)
 		die($e->getMessage());
 	}
 }
-function PrintTableCols($tableName,$connect,$colnames)
+function PrintTableCols($student,$connect,$name)
 {
 	try {
 
-		//Query
 		$sql = "SELECT ";
-		for ($i=0; $i < sizeof($colnames)-1; $i++) { 
-			$sql .=$colnames[$i].",";
+		for ($i=0; $i < sizeof($name)-1; $i++) { 
+			$sql .=$name[$i].",";
 			//echo $sql;
 		}
-		$sql .=$colnames[sizeof($colnames)-1]." from $tableName";
-		//echo $sql;
-		//excute the quey
+		$sql .=$name[sizeof($name)-1]." from $student";
 
 		$result = mysqli_query($connect,$sql);
-		//check if data exists in the table
 		if (mysqli_num_rows($result)>0) {
-			// fetch the data from rows
 			echo "<table border='1'>";
 			$col = mysqli_fetch_fields($result);
-			//print the colums
 			echo "<tr>";
 			foreach ($col as $value) {
-				//return an object
-				//print_r($value);
 				echo "<td>$value->name</td>";
 			}
 			echo "</tr>";
 			
 			while ($row = mysqli_fetch_assoc($result)) {
-				//print the data in a table format
 				echo "<tr>";
 				foreach ($row as $key => $value) {
 					echo "<td>$value</td>";
@@ -87,32 +71,22 @@ function PrintTableCols($tableName,$connect,$colnames)
 	}
 }
 
-//wrtie a function to search the books by name
-function SearchBooks($name,$connect)
+function Searchname($Male,$connect)
 {
 	try {
 
-	//Query
-		$sql = "SELECT * FROM books where title like '%$name%' ";
-		//echo $sql;
-	//excute the quey
+		$sql = "SELECT * FROM student where gender like '%$Male%' ";
 		$result = mysqli_query($connect,$sql);
-	//check if data exists in the table
 		if (mysqli_num_rows($result)>0) {
-		// fetch the data from rows
 			echo "<table border='1'>";
 			$col = mysqli_fetch_fields($result);
-		//print the colums
 			echo "<tr>";
 			foreach ($col as $value) {
-			//return an object
-			//print_r($value);
 				echo "<td>$value->name</td>";
 			}
 			echo "</tr>";
 			
 			while ($row = mysqli_fetch_assoc($result)) {
-			//print the data in a table format
 				echo "<tr>";
 				foreach ($row as $key => $value) {
 					echo "<td>$value</td>";
